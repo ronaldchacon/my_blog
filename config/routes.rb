9784c5fe12ea60d6c1311e63a6402ea8b47fb085
Ronald Chacon <ronald.chacon87@gmail.com>
2015-07-08 01:14:31 -0700

    Update devise routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   devise_for :admins
+  devise_scope :admin do
+    get 'admin', to: 'devise/registrations#new'
+  end
   resources :posts
   root to: 'posts#index'
 end
