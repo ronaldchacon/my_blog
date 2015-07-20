1297885e393e562174478418e589e24059ae376e
Ronald Chacon <ronald.chacon87@gmail.com>
2015-07-20 14:23:18 -0700

    Update admin styles, update routes, update links

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :admins
   devise_scope :admin do
-    get 'admin', to: 'devise/registrations#new'
+    get 'admin', to: 'devise/sessions#new'
   end
   resources :posts
   root to: 'posts#index'
